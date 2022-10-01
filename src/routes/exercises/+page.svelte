1d4ee6876ddae7b88a65c7b8a1d387ea5f57bd9e
mateosayas <mateosayaspereira@gmail.com>
2022-10-01 00:13:55 -0300

    feat: WIP list excercises

---
+++ src/routes/exercises/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import exercises from '../../../static/exercises.json';
+
+	let keys = Object.keys(exercises);
+	let values = Object.values(exercises);
+	let entries = Object.entries(exercises);
+</script>
+
+<svelte:head>
+	<title>Excercises - FitSchedule</title>
+</svelte:head>
+
+<div class="excercises-container">
+	<h1>Excercises</h1>
+	<!-- {#each entries as [key, value]}
+		<div>{key} - {value}</div>
+	{/each} -->
+	{#each keys as key}
+		<p>{key}</p>
+	{/each}
+</div>
